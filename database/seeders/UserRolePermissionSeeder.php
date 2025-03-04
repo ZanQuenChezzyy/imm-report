0c269e96c9e511e99e484469478da75ea70e6806
ZanQuen <andereyan@gmail.com>
2025-03-04 23:17:19 +0800

    initialize: new seeder

--- database/seeders/UserRolePermissionSeeder.php
+++ database/seeders/UserRolePermissionSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
 use App\Models\User;
+use App\Models\UserCompany;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -16,6 +18,9 @@ class UserRolePermissionSeeder extends Seeder
      */
     public function run(): void
     {
+        // Create or get the default company
+        $company = Company::firstOrCreate(['name' => 'PT. Indominco Mandiri']);
+
         // Permissions for users
         $permissions = [
             'View Any User',
@@ -77,56 +82,7 @@ class UserRolePermissionSeeder extends Seeder
 
         // Create roles
         $roles = [
-            'Administrator' => [
-                'View Any User',
-                'View User',
-                'Create User',
-                'Update User',
-                'Delete User',
-                'Restore User',
-                'Force Delete User',
-                'View Any Role',
-                'View Role',
-                'Create Role',
-                'Update Role',
-                'Delete Role',
-                'Restore Role',
-                'Force Delete Role',
-                'View Any Permission',
-                'View Permission',
-                'Create Permission',
-                'Update Permission',
-                'Delete Permission',
-                'Restore Permission',
-                'Force Delete Permission',
-
-                // Permissions for Company
-                'View Any Company',
-                'View Company',
-                'Create Company',
-                'Update Company',
-                'Delete Company',
-                'Restore Company',
-                'Force Delete Company',
-
-                // Permissions for Report Type
-                'View Any Report Type',
-                'View Report Type',
-                'Create Report Type',
-                'Update Report Type',
-                'Delete Report Type',
-                'Restore Report Type',
-                'Force Delete Report Type',
-
-                // Permissions for Report
-                'View Any Report',
-                'View Report',
-                'Create Report',
-                'Update Report',
-                'Delete Report',
-                'Restore Report',
-                'Force Delete Report',
-            ],
+            'Administrator' => $permissions, // Admin memiliki semua permissions
             'Kontraktor' => [
                 'View Any Report',
                 'View Report',
@@ -169,8 +125,14 @@ class UserRolePermissionSeeder extends Seeder
             );
 
             $user->assignRole($userData['role']);
+
+            // Hubungkan user ke perusahaan PT. Indominco Mandiri
+            UserCompany::firstOrCreate([
+                'user_id' => $user->id,
+                'company_id' => $company->id,
+            ]);
         }
 
-        $this->command->info('Roles, Permissions, dan Users Telah berhasil dibuat!');
+        $this->command->info('Roles, Permissions, Users, dan Company telah berhasil dibuat!');
     }
 }
